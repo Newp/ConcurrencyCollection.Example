ebb3646efa46b431194dc04ff51b387ddad9b9c4
Newp <mynewp@gmail.com>
2021-05-13 22:11:14 +0900

    ConcurrentBag에 대한 예제코드 추가

--- ConcurrencyCollection.Example/ConcurrencyCollection.Example/Program.cs
+++ ConcurrencyCollection.Example/ConcurrencyCollection.Example/Program.cs
@@ -1,37 +1,38 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace ConcurrencyCollection.Example
+namespace DictionaryExample
 {
     class Program
     {
+        /*
+         아래 코드의 주석을 사용하여 ConcurrentBag과 List를 번갈아 사용해보세요.
+         */
         static readonly ConcurrentBag<string> bag = new ConcurrentBag<string>();
-        
-        static async Task Main(string[] args)
-        {
-            bag.Add("a");
-            bag.Add("b");
-            bag.Add("c"); ;
-            bag.Add("d"); ;
-            bag.Add("e");
+        //static readonly List<string> bag = new List<string>();
 
+        static void Main(string[] args)
+        {
             var cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.CancelAfter(1500);
 
-            _ = ReadTest(cancellationTokenSource.Token);
+            Task.WaitAll(
+                Add(cancellationTokenSource.Token),
+                Read(cancellationTokenSource.Token)
+            );
 
-            await Task.Delay(500);
-            
-            cancellationTokenSource.Cancel();
-            await Task.Delay(3000);
+            var msg = string.Join(string.Empty, bag.ToArray());
+            Console.WriteLine(msg.Length);
+         }
 
-        }
-
-        static async Task ReadTest(CancellationToken cancellationToken)
+        static async Task Read(CancellationToken cancellationToken)
         {
-            while(true)
+            await Task.Delay(1);
+            while (true)
             {
                 if (cancellationToken.IsCancellationRequested)
                 {
@@ -39,13 +40,24 @@ namespace ConcurrencyCollection.Example
                     break;
                 }
 
-                await Task.Delay(0, cancellationToken);
-
-                var xx = string.Join(' ', bag.ToArray());
-
-                Console.WriteLine($"{DateTime.Now:HH:mm.ss.fff}>>{xx}");
+                foreach (var item in bag) { } //아무것도 안하고 반복자만 수행해도 동시성 보장이 안되면 문제가 생김.
             }
+        }
 
+        static async Task Add(CancellationToken cancellationToken)
+        {
+            await Task.Delay(1);
+            while (true)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    Console.WriteLine("cancel requested");
+                    break;
+                }
+
+                bag.Add("!");
+                //await Task.Delay(1);
+            }
         }
     }
 }
